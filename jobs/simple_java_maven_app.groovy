eaf86a48604b93006e35eec47b195455b582fcd2
Simon McCartney <simon.mccartney@hpe.com>
2017-12-30 12:17:01 +0000

    Don't scan GitHub for the branches

--- jobs/simple_java_maven_app.groovy
+++ jobs/simple_java_maven_app.groovy
@@ -1,6 +1,4 @@
 def project = 'jenkins201/simple-java-maven-app'
-def branchApi = new URL("https://api.github.com/repos/${project}/branches")
-def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 
 def branchName = 'master'
 def jobName = "${project}-${branchName}".replaceAll('/','-')
