476590264383f73b580c9739aa6233cb84e0ecd5
Simon McCartney <simon.mccartney@hpe.com>
2017-12-30 00:03:26 +0000

    Use the blueocean image as a starting point

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
-FROM jenkins/jenkins:lts
-COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
-RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt && /usr/local/bin/install-plugins.sh docker-slaves github-branch-source
+FROM jenkinsci/blueocean
+# Copy our seed job in to the container - this will then get persisted to
+# the volume created on demand the first time we start the container with a
+# volume mounted into /var/jenkins_home
+COPY jobs/jenkins-container-seed-job/config.xml /var/jenkins_home/jobs/jenkins-container-seed-job/config.xml
+RUN /usr/local/bin/install-plugins.sh job-dsl docker-slaves github-branch-source
--- run.sh
+++ run.sh
@@ -1,7 +1,21 @@
 #!/bin/bash -x
-docker pull jenkins/jenkins:lts
-docker build -t jenkins201/jenkins-container:latest .
-docker run -p 8080:8080 -p 50000:50000 -v ${PWD}:/var/jenkins_home jenkins201/jenkins-container:latest
+#
 
-# open the UI
-# open http://127.0.0.1:8080
+TARGET_CONTAINER="jenkins201/jenkins-container:latest"
+
+docker build -t ${TARGET_CONTAINER} .
+
+docker run \
+  --rm \
+  -u root \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$HOME":/home \
+  --name jenkins201_jenkins \
+  ${TARGET_CONTAINER}
+
+docker rm jenkins201_jenkins
+docker rmi ${TARGET_CONTAINER}
+docker volume rm jenkins-data
