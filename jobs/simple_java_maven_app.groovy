66abc741dd1657daa7aabfb75b89fa007d422bed
Simon McCartney <simon.mccartney@hpe.com>
2017-12-30 21:49:15 +0000

    Replace simple-java-maven-app freestyle job with a pipeline job

--- jobs/simple_java_maven_app.groovy
+++ jobs/simple_java_maven_app.groovy
@@ -1,19 +1,20 @@
 def project = 'jenkins201/simple-java-maven-app'
 def branchName = 'master'
-def jobName = "${project}-${branchName}".replaceAll('/','-')
+def jobName = "${project}".replaceAll('/','-')
 
-job(jobName) {
-  scm {
-    // Pass the parameters via a closure block to avoid the Job DSL default of creating a tag
-    // https://stackoverflow.com/questions/36556572/jenkins-job-dsl-always-creates-a-git-tag-for-freestyle-jobs-using-the-git-plugin
-    git {
-      remote {
-        url("git://github.com/${project}.git")
-      }
-      branch(branchName)
+pipelineJob(jobName) {
+    definition {
+        cpsScm {
+            scm {
+								// Pass the parameters via a closure block to avoid the Job DSL default of creating a tag
+								// https://stackoverflow.com/questions/36556572/jenkins-job-dsl-always-creates-a-git-tag-for-freestyle-jobs-using-the-git-plugin
+								git {
+									remote {
+										url("git://github.com/${project}.git")
+									}
+									branch(branchName)
+								}
+            }
+        }
     }
-  }
-  steps {
-    maven("test -Dproject.name=${project}/${branchName}")
-  }
 }
