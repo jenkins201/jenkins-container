537db40bcf30ffa24e6149aa5cabd806e14415a6
Simon McCartney <simon.mccartney@hpe.com>
2017-12-30 21:21:59 +0000

    Disable git tag creation

--- jobs/simple_java_maven_app.groovy
+++ jobs/simple_java_maven_app.groovy
@@ -1,13 +1,19 @@
 def project = 'jenkins201/simple-java-maven-app'
-
 def branchName = 'master'
 def jobName = "${project}-${branchName}".replaceAll('/','-')
 
 job(jobName) {
-    scm {
-        git("git://github.com/${project}.git", branchName)
+  scm {
+    // Pass the parameters via a closure block to avoid the Job DSL default of creating a tag
+    // https://stackoverflow.com/questions/36556572/jenkins-job-dsl-always-creates-a-git-tag-for-freestyle-jobs-using-the-git-plugin
+    git {
+      remote {
+        url("git://github.com/${project}.git")
+      }
+      branch(branchName)
     }
-    steps {
-        maven("test -Dproject.name=${project}/${branchName}")
-    }
+  }
+  steps {
+    maven("test -Dproject.name=${project}/${branchName}")
+  }
 }
