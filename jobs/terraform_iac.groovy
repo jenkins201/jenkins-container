6b9cf5a19d45241fa74b3aba96bf51aedb8620fe
Simon McCartney <simon.mccartney@taskize.com>
2018-06-14 23:23:44 +0100

    Added terraform-iac

---
+++ jobs/terraform_iac.groovy
@@ -0,0 +1,22 @@
+def project = 'jenkins201/terraform-iac'
+def branchName = 'master'
+def jobName = "${project}".replaceAll('/','-')
+
+pipelineJob(jobName) {
+    definition {
+        cpsScm {
+            scm {
+                // Pass the parameters via a closure block to avoid the Job DSL default of creating a tag
+                // https://stackoverflow.com/questions/36556572/jenkins-job-dsl-always-creates-a-git-tag-for-freestyle-jobs-using-the-git-plugin
+                git {
+                  remote {
+                    url("git://github.com/${project}.git")
+                  }
+                  branch(branchName)
+                  // Jenkinsfile or JenkinsFile?
+                  scriptPath('Jenkinsfile')
+                }
+            }
+        }
+    }
+}
